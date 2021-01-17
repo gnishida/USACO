b0daf6a0924dced6a7895933fd008ceb97347d79
Gen Nishida <gen.nishida@gmail.com>
2021-01-17 13:13:56 -0800

    Updated

--- Gold/202002/Problem2/help.cpp
+++ Gold/202002/Problem2/help.cpp
@@ -7,18 +7,6 @@ using namespace std;
 
 const long long MOD = 1000000007;
 
-long long mypow(long long num, long long e) {
-    if (e == 1) return num;
-
-    long long h = e / 2;
-    long long ret = mypow(num, h);
-    ret = (ret * ret) % MOD;
-    if (e % 2 == 1) {
-        ret = (ret * num) % MOD;
-    }
-    return ret;
-}
-
 int main() {
     ifstream in("help.in");
     int N;
@@ -40,21 +28,21 @@ int main() {
         const auto& type = events[i - 1].second.second;
         if (type == 1) {
             k++;
-            DP[i][0] = (DP[i - 1][0] * 2 + DP[i - 1][2]) % MOD;
-            DP[i][1] = (DP[i - 1][1] * 2 + 1) % MOD;
-            DP[i][2] = DP[i - 1][2];
-            DP[i][3] = (mypow(2, k) - 1 - DP[i - 1][2] + MOD) % MOD;
+            DP[i][0] = (DP[i - 1][0] * 2 + DP[i - 1][1] + DP[i - 1][3] + 1) % MOD;
+            DP[i][1] = DP[i - 1][1];
+            DP[i][2] = (DP[i - 1][2] * 2 + DP[i - 1][3] + 1) % MOD;
+            DP[i][3] = DP[i - 1][3];
         } else {
             k--;
-            DP[i][0] = (DP[i - 1][0] + 1) % MOD;
-            DP[i][1] = (DP[i - 1][1] + MOD - 1) % MOD;
-            DP[i][2] = (DP[i - 1][2] * 2 + 1) % MOD;
-            DP[i][3] = (DP[i - 1][2] + DP[i - 1][3] - DP[i][2] + MOD) % MOD;
+            DP[i][0] = (DP[i - 1][0] - DP[i - 1][1] - DP[i - 1][3] - 1) % MOD;
+            DP[i][1] = (DP[i - 1][1] * 2 + DP[i - 1][3] + 1) % MOD;
+            DP[i][2] = (DP[i - 1][2] - DP[i - 1][3]  - 1) % MOD;
+            DP[i][3] = (DP[i - 1][3] * 2 + 1) % MOD;
         }
     }
 
     ofstream out("help.out");
-    out << (DP[N * 2][0] + DP[N * 2][1]) % MOD << endl;
+    out << ((DP[N * 2][0] + DP[N * 2][1]) % MOD + MOD) % MOD << endl;
 
     return 0;
 }
